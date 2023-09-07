c32ae9f8b3f153774fac4e571d37e39f5913b4f1
farrisismatiteach <farris.ismati.teach@gmail.com>
2023-09-07 10:38:44 -0400

    added alt list item

---
+++ src/components/DesignSystem/List/ListRowAlt.tsx
@@ -0,0 +1,29 @@
+import { SemanticSetCores, getColorSet } from "@/styles/colors";
+import React from "react";
+import { styled } from "styled-components";
+import ListRow, { ListProps } from "./ListRow";
+
+const StyledListRow = styled(ListRow)`
+	background-color: transparent;
+`;
+
+export default React.forwardRef<HTMLElement, ListProps>(function List(
+	{
+		children,
+		colorSet = getColorSet(SemanticSetCores.SECONDARY),
+		supportingText,
+		...props
+	},
+	ref
+) {
+	return (
+		<StyledListRow
+			ref={ref}
+			{...props}
+			colorSet={colorSet}
+			supportingText={supportingText}
+		>
+			{children}
+		</StyledListRow>
+	);
+});
--- src/components/DesignSystem/List/index.stories.tsx
+++ src/components/DesignSystem/List/index.stories.tsx
@@ -1,6 +1,7 @@
 import { spacer32 } from "@/styles/sizes";
 import List from ".";
 import ListRow from "./ListRow";
+import ListRowAlt from "./ListRowAlt";
 export default {
 	component: List,
 	title: "List",
@@ -36,5 +37,37 @@ export const MultipleListItemsCustomGap = () => (
 		<ListRow>List Item 3</ListRow>
 		<ListRow supportingText="Supporting Text">List Item 4</ListRow>
 		<ListRow>List Item 5</ListRow>
+	</List>
+);
+
+export const DefaultAlt = () => (
+	<List>
+		<ListRowAlt>List Item</ListRowAlt>
+	</List>
+);
+
+export const SupportingTextAlt = () => (
+	<List>
+		<ListRowAlt supportingText="Supporting Text">List Item</ListRowAlt>
+	</List>
+);
+
+export const MultipleListItemsDefaultGapAlt = () => (
+	<List>
+		<ListRowAlt>List Item 1</ListRowAlt>
+		<ListRowAlt supportingText="Supporting Text">List Item 2</ListRowAlt>
+		<ListRowAlt>List Item 3</ListRowAlt>
+		<ListRowAlt supportingText="Supporting Text">List Item 4</ListRowAlt>
+		<ListRowAlt>List Item 5</ListRowAlt>
+	</List>
+);
+
+export const MultipleListItemsCustomGapAlt = () => (
+	<List gap={spacer32}>
+		<ListRowAlt>List Item 1</ListRowAlt>
+		<ListRowAlt supportingText="Supporting Text">List Item 2</ListRowAlt>
+		<ListRowAlt>List Item 3</ListRowAlt>
+		<ListRowAlt supportingText="Supporting Text">List Item 4</ListRowAlt>
+		<ListRowAlt>List Item 5</ListRowAlt>
 	</List>
 );
